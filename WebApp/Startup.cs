8a3649de33e2b868c298817380fe7c3c39951343
trinhvinhtruong96 <48130078+trinhvinhtruong96@users.noreply.github.com>
2020-04-27 21:51:07 +0700

    aaaaaaaa

--- WebApp/Startup.cs
+++ WebApp/Startup.cs
@@ -94,6 +94,9 @@ namespace WebApp
                 routes.MapRoute(
                     name: "default",
                     template: "{controller=Home}/{action=Index}/{id?}");
+                routes.MapRoute(
+                    name: "areaRoute",
+                    template: "{area:exists}/{controller=Home}/{action=Index}/{id?}");
             });
         }
     }
